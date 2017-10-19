dac47d75af6f4ed16e1fb7a63dee30eda8d30925
Gary Fredericks <fredericksgary@gmail.com>
2017-10-19 17:15:53 -0500

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.gfredericks/lein-all-my-files-should-end-with-exactly-one-newline-character "0.1.0-SNAPSHOT"
+(defproject com.gfredericks/lein-all-my-files-should-end-with-exactly-one-newline-character "0.1.0"
   :description "A Clojure linter that enforces end-of-file newlines."
   :url "https://github.com/gfredericks/lein-all-my-files-should-end-with-exactly-one-newline-character"
   :license {:name "Eclipse Public License"
